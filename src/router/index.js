0ccfc7ac12811c5c7a414ad628138277fd5a6bf9
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-07-28 14:04:45 -0400

    switch history mode so spa

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createMemoryHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(),
   routes: [
     {
       path: '/',
@@ -13,9 +14,6 @@ const router = createRouter({
     {
       path: '/tutorial',
       name: 'tutorial',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Tutorial.vue')
     }, 
     {
