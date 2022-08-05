a9401f247514244af11a5389c6709d7dd94c02b7
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-08-04 21:05:49 -0400

    tutorail buidl out - still need menu nav.

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,47 +1,49 @@
 <script setup>
-import { computed } from '@vue/reactivity';
+import { computed } from '@vue/reactivity'
 
-
-const props = defineProps(['tuts']);
-
-
-
-
+const props = defineProps(['tuts'])
 </script>
 
 <template>
-
-<div class="progress-bars">
-
-    <div v-for="(bars, index) in props.tuts.videos" :key="index" class="progress-bar">
-    <div class="progress-bar-inner" :style="{ width: bars.marker + '%' }"></div>
+  <div class="progress-bars">
+    <div
+      v-for="(bars, index) in props.tuts.videos"
+      :key="index"
+      class="progress-bar"
+    >
+      <div
+        class="progress-bar-inner"
+        :style="{ width: bars.marker + '%' }"
+      ></div>
     </div>
-</div>
+  </div>
 </template>
 
 <style scoped>
-
 .progress-bars {
-    display: flex;
-    flex-flow: row nowrap;
-    gap: 15px;
-    width: 100%;
-    justify-content: space-around;
-    margin-bottom: 50px;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 15px;
+  width: 100%;
+  justify-content: space-around;
+  padding: 10px;
+  position: absolute;
+  z-index: 99999;
+  background-color: var(--color-green-darkest);
 }
 
 .progress-bar {
-    width: 100%;
-    height: 15px;
-    background-color: var(--color-green);
-    /* margin: 10px; */
+  width: 100%;
+  height: 15px;
+  background-color: #809e8f;
+  border-radius: 10px;
 }
 
 .progress-bar-inner {
-    height: 100%;
-    background-color: var(--color-green-dark);
-    width: 0;
-    transition: width 0.5s;
+  height: 100%;
+  background-color: var(--vt-c-white);
+  width: 0;
+  transition: width 0.5s;
+  border-radius: 10px;
 }
-
 </style>
