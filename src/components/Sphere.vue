b3f68da22167c8c2e9259bff04cb5e3f7dfb53ee
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-08-10 15:57:24 -0400

    home view built and animated

---
+++ src/components/Sphere.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+import Logo from '../assets/pick3plusfireball-logo.png';
+
+onMounted(() => {
+  let spheretl = gsap.timeline({});
+  spheretl.to('#sphere', { duration: 1 });
+  spheretl.to('#sphere', { duration: 0.5, scale: 1, ease: 'power1.inOut' });
+
+  let sphereRotateY = gsap.timeline({
+    repeat: -1,
+    delay: 2,
+});
+    sphereRotateY.to('#spherelogo', { delay: -0.5, duration: 1.2, opacity: 1, scale: 1, y: 0, ease: 'power2.out' });
+    sphereRotateY.to('#spherelogo', { delay: 1.2, duration: 1.2, opacity: 0, scale: 0.85, y: '100%', ease: 'power2.in' });
+    sphereRotateY.to('#spherelogo', { delay: 0, duration: 0, y: '-100%' });
+    sphereRotateY.to('#spheretext', { delay: -0.5, duration: 1.2, opacity: 1, scale: 1, y: 0, ease: 'power2.out' });
+    sphereRotateY.to('#spheretext', { delay: 1.2, duration: 1.0, opacity: 0, scale: 0.75, y: '100%', ease: 'power2.in' });
+    sphereRotateY.to('#spheretext', { delay: 0, duration: 0, y: '-100%' });
+});
+
+
+</script>
+
+<template>
+    <div class="sphere">
+      <div id="sphere" class="sphere__inner">
+        <div id="spherelogo" class="sphere__logo">
+          <img width="135" height="53" id="logo" alt="Pick 3 Plus Fireball logo" :src="Logo" />
+        </div>
+        <div id="spheretext" class="sphere__text">
+          The Fun
+          <br />
+          Starts Here.
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.sphere {
+    margin: 40px auto;
+    ;
+}
+.sphere__inner {
+  position: relative;
+  width: 182px;
+  height: 182px;
+  margin: 0 auto;
+  border-radius: 50%;
+  background: radial-gradient(
+    56% 56% at 53.18% 22.76%,
+    #33b06e 0%,
+    #2c965e 59%,
+    #247d4e 72%,
+    #1d633e 87%,
+    #1d633e 100%
+  );
+  background-repeat: no-repeat;
+  overflow: hidden;
+  transform: scale(20);
+}
+
+.sphere__logo,
+.sphere__text {
+  transform: scale(0.85) translateY(-100%) translateZ(1px);
+}
+
+.sphere__logo, .sphere__text {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 24px;
+  line-height: 0.923;
+  color: var(--vt-c-white);
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,202 @@
 <script setup>
+import { onMounted } from 'vue'
+import gsap from 'gsap'
+import BgStars from '../components/BgStars.vue'
+import Sphere from '../components/Sphere.vue'
+import { useRouter, useRoute } from 'vue-router'
+import Greenstar from '../assets/star-green.svg'
+import Higreenstar from '../assets/star-higreen.svg'
+import Goldstar from '../assets/star-gold.svg'
+
+const router = useRouter()
+const route = useRoute()
+
+onMounted(() => {
+  let loadInTL = gsap.timeline({delay: 2,})
+  loadInTL.to('#tutbtn', {delay: -0.2,duration: 0.4,opacity: 1,scale: 1,'pointer-events': 'all',})
+  loadInTL.to('#demobtn', {delay: -0.2,duration: 0.4,opacity: 1,scale: 1,'pointer-events': 'all',})
+  loadInTL.to('#tutbtntxt', { delay: -0.1, duration: 0.6, opacity: 1, scale: 1, 'pointer-events': 'all' });
+  loadInTL.to('#demobtntxt', { delay: -0.1, duration: 0.6, opacity: 1, scale: 1, 'pointer-events': 'all' });
+  loadInTL.to('#sound-warning', {delay: -0.2,duration: 0.6,opacity: 1, y: 0})
+  loadInTL.to('#age-warning', { delay: 0, duration: 0.6, opacity: 1, y: 0 })
+});
+
+const emit = defineEmits(['music']);
+
+const onLeave = (path) => {
+  if(path == 'tutorial') {
+    console.log('one')
+      emit('music', '/audio/3FunkShortVersion.mp3')
+  }
+  else {
+    console.log('two')
+        emit('music', 'audio/FunkyInFunky.mp3')
+  }
+  let loadOutTL = gsap.timeline({delay: 0.5})
+      loadOutTL.to('#tutbtntxt', { delay: 0, duration: 0.4, opacity: 0, 'pointer-events': 'none' });
+      loadOutTL.to('#demobtntxt', { delay: -0.4, duration: 0.4, opacity: 0, 'pointer-events': 'none' });
+      loadOutTL.to('#sound-warning', { delay: -0.3, duration: 0.4, opacity: 0 });
+      loadOutTL.to('#age-warning', { delay: -0.3, duration: 0.4, opacity: 0 });
+      loadOutTL.to('#tutbtn', { delay: -0.3, duration: 0.2, opacity: 0 });
+      loadOutTL.to('#demobtn', { delay: -0.2, duration: 0.2, opacity: 0 });
+      loadOutTL.to('.sphere', { delay: -0.1, duration: 0.5, scale: 0 });
+      loadOutTL.to('.transition-stars', { delay: 0, duration: 0, opacity: 1 });
+      loadOutTL.to('.star__green', { delay: 0, duration: 0, opacity: 1 });
+      loadOutTL.to('.star__gold', { delay: 0, duration: 0, opacity: 1 });
+      loadOutTL.to('.star__higreen', { delay: 0, duration: 0, opacity: 1 });
+      loadOutTL.to('.star__green', { delay: 0, duration: 1.1, scale: 45, ease: 'expo.in' });
+      loadOutTL.to('.star__gold', { delay: -0.8, duration: 0.75, scale: 45, ease: 'expo.in' });
+      loadOutTL.to('.star__higreen', { delay: -0.5, duration: 0.6, scale: 45, ease: 'expo.in' });
+      loadOutTL.call(changePage);
+
+  function changePage() {
+  router.replace({ name: path })
+  }
+}
+
+
+
+
 </script>
 
 <template>
   <main>
-    Hello, I'm Main Home view.
+    <Sphere :class="{ sphere }" />
+    <a id="tutbtn" href @click.prevent="onLeave('tutorial')" class="accent-button">
+      <div id="tutbtntxt">
+        <span class="button__title">Learn the Game</span>
+        <span class="button__subtitle">Take the tour</span>
+      </div>
+    </a>
+
+    <a id="demobtn" href @click.prevent="onLeave('game')" class="accent-button">
+      <div id="demobtntxt">
+        <span class="button__title">Try the Game</span>
+        <span class="button__subtitle">Play Demo</span>
+      </div>
+    </a>
+    <div class="home-warnings">
+      <p id="sound-warning">This experience has sound</p>
+      <p id="age-warning">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+    </div>
+    <div class="transition-stars">
+    <img class="transition-stars__star star__green" width="124.4" height="124.4" :src=Greenstar alt="star">
+    <img class="transition-stars__star star__gold" width="124.4" height="124.4" :src=Goldstar alt="star">
+    <img class="transition-stars__star star__higreen" width="124.4" height="124.4" :src=Higreenstar alt="star">
+</div>
   </main>
+  <BgStars gray="true" />
 </template>
+
+<style scoped>
+a.accent-button {
+  padding: 8px 24px 10px;
+  background: var(--color-green-accent);
+  box-shadow: 0px 4px 0px 0px var(--color-green),
+    0px 0px 4px 0px var(--color-green);
+  border-radius: 8px;
+  border-width: 0;
+  color: var(--vt-c-white);
+  margin: 24px 32px 0;
+  min-width: 300px;
+  display:block;
+}
+
+a.accent-button div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+a.accent-button:hover {
+  background: var(--color-green-light);
+}
+
+.button__title {
+  font-style: italic;
+  font-weight: 900;
+  font-size: 28px;
+  line-height: 1.2;
+  text-align: center;
+  text-shadow: -1px 1px 1px var(--color-green-darkest);
+}
+
+.button__subtitle {
+  font-size: 14px;
+  line-height: 1.2;
+  text-align: center;
+  letter-spacing: -0.35px;
+  text-shadow: -1px 1px 1px var(--color-green-darkest);
+}
+
+.button__subtitle:before,
+.button__subtitle:after {
+  content: '';
+  display: block;
+  height: 10px;
+  width: 10px;
+  background: url(../assets/goldstar.svg) no-repeat center;
+  background-size: contain;
+  position: absolute;
+  top: calc(50% - 3px);
+  left: -15px;
+}
+
+.button__subtitle:after {
+  left: auto;
+  right: -15px;
+}
+
+.home-warnings {
+  margin: 20px 30px 0;
+  text-align: center;
+}
+
+#sound-warning {
+  font-size: 11px;
+  font-weight: 900;
+  margin-bottom: 10px;
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+#age-warning {
+  font-size: 11px;
+  line-height: 1.5;
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+#tutbtn,
+#demobtn,
+#tutbtntxt,
+#demobtntxt {
+  opacity: 0;
+  transform: scale(0.8);
+  pointer-events: none;
+}
+
+.transition-stars {
+  position: absolute;
+  pointer-events: none;
+  top: 126px;
+  left: calc(50% - 62px);
+}
+
+.transition-stars__star {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: scale(0);
+}
+
+.star__green, .star__gray, .star__gold, .star__higreen {
+  opacity: 0;
+}
+</style>
