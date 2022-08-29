978f1cba6fc363d4819e58ab59f6df98c3b5d838
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-08-29 15:30:21 -0400

    particles main js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Particles from "particles.vue3";
+
 
 import './assets/main.css'
 import { createPinia } from 'pinia'
 
-const app = createApp(App).use(createPinia())
+const app = createApp(App).use(createPinia()).use(Particles)
 
 app.use(router)
 
