9c0fd634f406640a66c9a72721e88ba78f3742ca
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-09-27 16:03:18 -0400

    calcwinners first pass

--- src/composables/calcWinners.js
+++ src/composables/calcWinners.js
@@ -11,42 +11,42 @@ export function calcWinners() {
       if (store.winpercentage < 75) {
         store.finalwinners = store.picks;
       } else {
-        store.finalwinners = [store.picks[0], store.picks[1], store.fireball];
+        store.finalwinners = [store.picks[0], store.picks[1], store.picks[2], store.fireball];
       }
     } else {
       if (store.winpercentage < 75) {
-        store.finalwinners = [store.picks[2], store.picks[0], store.picks[1]];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.picks[1]];
       } else {
-        store.finalwinners = [store.picks[2], store.picks[0], store.fireball];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.fireball];
       }
     }
   } else {
     if (store.presentgame === "exact") {
       if (store.winpercentage < 25) {
         store.finalwinners = store.picks;
-        store.finalfireball = store.picks[2];
+        store.finalfireball = store.picks[3];
       } else if (store.winpercentage < 50) {
-        store.finalwinners = [store.picks[0], store.picks[1], store.fireball];
-        store.finalfireball = store.picks[2];
+        store.finalwinners = [store.picks[0], store.picks[1], store.picks[2], store.fireball];
+        store.finalfireball = store.picks[3];
       } else if (store.winpercentage < 75) {
         store.finalwinners = store.picks;
         store.finalfireball = store.fireball;
       } else {
-        store.finalwinners = [store.picks[0], store.picks[1], store.loser];
+        store.finalwinners = [store.picks[0], store.picks[1], store.picks[2], store.loser];
         store.finalfireball = store.fireball;
       }
     } else if (store.presentgame === "any") {
       if (store.winpercentage < 25) {
-        store.finalwinners = [store.picks[2], store.picks[0], store.picks[1]];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.picks[1]];
         store.finalfireball = store.picks[1];
       } else if (store.winpercentage < 50) {
-        store.finalwinners = [store.picks[2], store.picks[0], store.fireball ];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.fireball ];
         store.finalfireball = store.picks[1];
       } else if (store.winpercentage < 75) {
-        store.finalwinners = [store.picks[2], store.picks[0], store.picks[1]];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.picks[1]];
         store.finalfireball = store.fireball;
       } else {
-        store.finalwinners = [store.picks[2], store.picks[0], store.loser];
+        store.finalwinners = [store.picks[2], store.picks[0], store.picks[3], store.loser];
         store.finalfireball = store.fireball;
       }
     }
