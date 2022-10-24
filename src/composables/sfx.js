07b132dfa904646ee945a2ed6762ae68e2c36537
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-10-24 11:50:05 -0400

    sound fx rework

--- src/composables/sfx.js
+++ src/composables/sfx.js
@@ -1,13 +1,14 @@
-const click = new Audio('../audio/sprite/button_click.mp3');
-const bubble = new Audio('../audio/sprite/bubble-pop.mp3');
-const jackpot = new Audio('../audio/sprite/jackpot.mp3');
-const failsound = new Audio('../audio/sprite/gamefail.mp3');
-const triumph = new Audio('../audio/sprite/triumph.mp3');
+// const click = new Audio('../audio/sprite/button_click.mp3');
+// const bubble = new Audio('../audio/sprite/bubble-pop.mp3');
+// const jackpot = new Audio('../audio/sprite/jackpot.mp3');
+// const failsound = new Audio('../audio/sprite/gamefail.mp3');
+// const triumph = new Audio('../audio/sprite/triumph.mp3');
 
 
 let loaded = false;
 export function ClickSound() {
     if (loaded) {
+        const click = new Audio('../audio/sprite/button_click.mp3');
         click.currentTime = 0;
         click.play();
     }
@@ -16,21 +17,25 @@ export function ClickSound() {
 
 
 export function Bubble() {
+    const bubble = new Audio('../audio/sprite/bubble-pop.mp3');
         bubble.currentTime = 0;
             bubble.play();
 }
   
 export function Jackpot() {
+    const jackpot = new Audio('../audio/sprite/jackpot.mp3');
     jackpot.currentTime = 0;
         jackpot.play();
 }
 
 export function FailSound() {
+    const failsound = new Audio('../audio/sprite/gamefail.mp3');
     failsound.currentTime = 0;
         failsound.play();
 }
 
 export function Triumph() {
+    const triumph = new Audio('../audio/sprite/triumph.mp3');
     triumph.currentTime = 0;
         triumph.play();
 }
