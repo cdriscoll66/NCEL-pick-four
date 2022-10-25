b03fbebd5bc8ee83ab649ffc5a517cecdfde0fdc
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-10-25 11:43:51 -0400

    changed better luck to new id

--- src/components/BetterLuck.vue
+++ src/components/BetterLuck.vue
@@ -15,13 +15,13 @@
         });
     
         // fade in system
-        tl.to('#hl1', {duration: .0, opacity: 0, y: 50});
+        tl.to('#blhl1', {duration: .0, opacity: 0, y: 50});
         tl.to('#logo', {duration: .0, opacity: 0, y: 50});
-        tl.to('#hl2', {duration: .0, opacity: 0, y: 50});
+        tl.to('#blhl2', {duration: .0, opacity: 0, y: 50});
     
-        tl.to ('#hl1', {duration: .3, opacity: 1, y: 0, ease: 'power1.inOut'});
+        tl.to ('#blhl1', {duration: .3, opacity: 1, y: 0, ease: 'power1.inOut'});
         tl.to ('#logo', {duration: .3, opacity: 1, y: 0, ease: 'power1.inOut'});
-        tl.to ('#hl2', {duration: .3, opacity: 1, y: 0, ease: 'power1.inOut'});
+        tl.to ('#blhl2', {duration: .3, opacity: 1, y: 0, ease: 'power1.inOut'});
      });
     
      const nextScreen = (num) => {
@@ -32,7 +32,7 @@
     
     <template>
     <div class="reward-wrapper">
-    <div><h2 id="hl1">You didn't win</h2></div>
+    <div><h2 id="blhl1">You didn't win</h2></div>
     <div><img
               width="256"
               height="102"
@@ -41,12 +41,12 @@
               :src="Logo"
             /></div>
         <div>
-            <a v-if="(store.fireballselected)" href @click.prevent="nextScreen(-1)" id="hl2" class="accent-button one-line">
+            <a v-if="(store.fireballselected)" href @click.prevent="nextScreen(-1)" id="blhl2" class="accent-button one-line">
             <div>
                 <span class="button__title">Play Fireball Drawing</span>
             </div>
             </a>
-            <h2 v-else class="betterluck" id="hl2">Better Luck Next Time!!!</h2>
+            <h2 v-else class="betterluck" id="blhl2">Better Luck Next Time!!!</h2>
         </div>
     
     
@@ -55,7 +55,7 @@
     
     <style scoped>
     
-    #hl1, #hl2, #logo {
+    #blhl1, #blhl2, #logo {
         opacity: 0;
         /* transform: translateY(50px); */
     }
